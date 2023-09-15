76766f9395907b6eec5c555c4a8dbf957b27207d
Andy <shikesenpu@gmail.com>
2023-09-15 15:05:01 +0200

    Added JWT issuing on program.cs

--- PUSGS_PR_162_2020/Program.cs
+++ PUSGS_PR_162_2020/Program.cs
@@ -1,12 +1,16 @@
 using AutoMapper;
 using EntityFramework.Exceptions.SqlServer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PUSGS_PR_162_2020.Infrastructure;
 using PUSGS_PR_162_2020.Interfaces;
 using PUSGS_PR_162_2020.Interfaces.RepoInterfaces;
 using PUSGS_PR_162_2020.Mapper;
 using PUSGS_PR_162_2020.Repository;
 using PUSGS_PR_162_2020.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +37,87 @@ IMapper mapper = config.CreateMapper();
 builder.Services.AddSingleton(mapper);
 #endregion
 
+#region SwaggerGen for JWT
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebshopServer", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "http://localhost:44319",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["secret"]))
+    };
+});
+
+//// Will I need cors?
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy(name: "cors", builder =>
+//    {
+//        builder
+//        .WithOrigins("http://localhost:3000")
+//        .AllowAnyHeader()
+//        .AllowAnyMethod()
+//        .AllowCredentials();
+//    });
+//});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("IsVerifiedSeller", policy => policy.RequireClaim("VerificationStatus", "Accepted"));
+});
+#endregion
+
+#region CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "cors", builder =>
+    {
+        builder
+        .WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+#endregion
+
 #region Adding all the Services
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
@@ -55,6 +140,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
